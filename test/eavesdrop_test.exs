915e7f41e6d40914d80fe299047e5895d489ebbf
supernullset <sean.j.williamson@gmail.com>
2016-10-09 11:42:48 -0700

    update test to run through the states trivially

--- test/eavesdrop_test.exs
+++ test/eavesdrop_test.exs
@@ -2,7 +2,15 @@ defmodule EavesdropTest do
   use ExUnit.Case
   doctest Eavesdrop
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Normal user state changes" do
+    {:ok, _pid} = Eavesdrop.Supervisor.start_link()
+
+    Eavesdrop.user_signin("test_user")
+    Eavesdrop.play_track("test_track")
+    Eavesdrop.user_stop
+    Eavesdrop.play_track("test_track 2")
+    Eavesdrop.user_signout
+
+    assert 1==1
   end
 end
